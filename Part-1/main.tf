cb3b95881006c41f3dd27b967f44423aa89a1757
kozha <ekoja86@gmail.com>
2023-12-13 21:10:54 +0500

    PrePreFinal

--- Part-1/main.tf
+++ Part-1/main.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
-      version = "0.100"# Фиксируем версию провайдера
+      version = "0.100"
     }
   }
 }
 provider "yandex" {
-  token     = "y0_AgAAAAAUGMM5AATuwQAAAADj5p1NwZol4arwRKKQIWj-DWzJPP9lYqg"
-  cloud_id  = "b1gb0d8kokqae5noef7s"
-  folder_id = "b1g07boj4j03d4ln6dhf"
+  token     = "<your_token>"
+  cloud_id  = "<your_cloud_id>"
+  folder_id = "<your_folder_id>"
   zone      = "ru-central1-a"
 }
 
@@ -24,29 +24,3 @@ resource "yandex_vpc_subnet" "k8s-subnet" {
   network_id     = yandex_vpc_network.k8s-network.id
   v4_cidr_blocks = ["10.2.0.0/16"]
 }
-
-/*
-module "k8s_worker" {
-  source                = "./modules/k8s/worker"
-  instance_family_image = "ubuntu-2204-lts"
-  vpc_subnet_id         = yandex_vpc_subnet.k8s-subnet.id
-}
-
-module "srv" {
-  source                = "./modules/srv"
-  instance_family_image = "ubuntu-2204-lts"
-  vpc_subnet_id         = yandex_vpc_subnet.subnet-1.id
-}
-
-output "kakoi_ip_master" {
-  value = module.k8s_master.external_ip_address_vm
-}
-
-output "kakoi_ip_worker" {
-  value = module.k8s_worker.external_ip_address_vm
-}
-
-output "kakoi_ip_monitoring" {
-  value = module.k8s_worker.external_ip_address_vm
-}
-*/
